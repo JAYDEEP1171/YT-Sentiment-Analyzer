6daec88fb471439d3bb2dc162ab2084a98c509c3
Jaydip Gohil <jaydeepgohil1171@gmail.com>
2025-05-12 00:27:07 +0530

    chnags 2

--- app.py
+++ app.py
@@ -292,5 +292,6 @@ def handle_exception(e):
 
 
 # --- Run the Flask App ---
-if __name__ == '__main__':
-    app.run(debug=True)  # Start the Flask development server
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
